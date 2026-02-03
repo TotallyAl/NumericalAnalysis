c272c685f7b9719d72f6b3d86d0d971651b104dd
TotallyAl <alexandre.detienne@gmail.com>
2026-02-03 17:55:23 +0100

    Code refactored

--- LinearSystems.py
+++ LinearSystems.py
@@ -1,99 +1,89 @@
 import numpy as np
 
-class LinearSystems:
+def backwardSubstitution(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    '''
+    Direct Method.
 
-    def __init__(self, A:np.ndarray, b:np.ndarray) -> None:
-        self.A = A
-        self.b = b
-
-    def _backwardSubstitution(self) -> np.ndarray:
-        '''
-        Direct Method.
-
-        Solves the system of linear equations Ax = b using backward substitution.
-        
-        Parameters:
-        A (np.ndarray): Coefficient matrix (assumed to be upper triangular, for now).
-        b (np.ndarray): Right-hand side vector.
-
-        Returns:
-        x (np.ndarray): Solution vector.
-        '''
-        n: int = len(self.b)
-        x: np.ndarray = np.zeros(n)
-        for i in range(n-1, -1, -1):
-            if i != n-1:
-                x[i] = (self.b[i] - sum(self.A[i, j] * x[j] for j in range(i+1, n))) / self.A[i, i]
-                continue
-            x[i] = self.b[i]/self.A[i, i]
-        return x.T
+    Solves the system of linear equations Ax = b using backward substitution.
     
-    def _swapLines(self, matrix: np.ndarray, line1: int, line2: int) -> None:
-        temp: np.ndarray = matrix[line1].copy()
-        matrix[line1] = matrix[line2]
-        matrix[line2] = temp
+    Parameters:
+    A (np.ndarray): Coefficient matrix (assumed to be upper triangular, for now).
+    b (np.ndarray): Right-hand side vector.
 
-    def _bestPivot(self, A: np.ndarray, b: np.ndarray , pivot: float, dimension: int) -> None:
-        for line in range(pivot, dimension):
-            if abs(A[line][pivot]) > abs(A[pivot][pivot]):
-                self._swapLines(A, line, pivot)
-                if b is not None:
-                    self._swapLines(b, line, pivot)
-
-    def _triangularize(self) -> np.ndarray:
-        '''
-        This method will transform a square matrix into an upper triangular matrix
-        '''
-        
-        A: np.ndarray = self.A.copy()
-        b: np.ndarray = self.b.copy()
-
-        dimension: int = len(A)
-        
-        for pivot in range(0, dimension):
-            self._bestPivot(A, b, pivot, dimension)
-            for row in range(pivot+1, dimension):
-                coeff: float = A[row][pivot] / A[pivot][pivot]
-                A[row] = A[row] - coeff * A[pivot]
-                b[row] = b[row] - coeff * b[pivot]
-        return A, b
-
-    def triangularSystem(self) -> np.ndarray:
-        return self._backwardSubstitution()
+    Returns:
+    x (np.ndarray): Solution vector.
+    '''
+    n: int = len(b)
+    x: np.ndarray = np.zeros(n)
+    for i in range(n-1, -1, -1):
+        if i != n-1:
+            x[i] = (b[i] - sum(A[i, j] * x[j] for j in range(i+1, n))) / A[i, i]
+            continue
+        x[i] = b[i]/A[i, i]
+    return x.T
     
-    def gauss(self) -> np.ndarray:
-        '''
-        Solving a linear system with Gauss
-        '''
-        self.A, self.b = self._triangularize()
-        return self.triangularSystem()
+def swapLines(matrix: np.ndarray, line1: int, line2: int) -> None:
+    temp: np.ndarray = matrix[line1].copy()
+    matrix[line1] = matrix[line2]
+    matrix[line2] = temp
 
-    def LUDecomposition(self) -> np.ndarray:
-        '''
-        Direct Method.
-        '''
+def bestPivot(A: np.ndarray, b: np.ndarray , pivot: float, dimension: int) -> None:
+    for line in range(pivot, dimension):
+        if abs(A[line][pivot]) > abs(A[pivot][pivot]):
+            swapLines(A, line, pivot)
+            if b is not None:
+                swapLines(b, line, pivot)
 
-        U: np.ndarray = self.A.copy()
-        dimension: int = len(U)
-        L: np.ndarray = np.eye(dimension)
-        
-        for pivot in range(0, dimension):
-            self._bestPivot(U, None, pivot, dimension)
-            for row in range(pivot+1, dimension):
-                coeff: float = U[row][pivot] / U[pivot][pivot]
-                U[row] = U[row] - coeff * U[pivot]
-                L[row][pivot] = coeff
-        return L, U
+def triangularize(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    '''
+    This method will transform a square matrix into an upper triangular matrix
+    '''
 
-    def jacobi(self) -> None:
-        '''
-        Iterative Method.
-        '''
-        
-        pass
+    dimension: int = len(A)
+    for pivot in range(0, dimension):
+        bestPivot(A, b, pivot, dimension)
+        for row in range(pivot+1, dimension):
+            coeff: float = A[row][pivot] / A[pivot][pivot]
+            A[row] = A[row] - coeff * A[pivot]
+            b[row] = b[row] - coeff * b[pivot]
+    return A, b
 
-    def gaussSeidel(self) -> None:
-        '''
-        Iterative Method.
-        '''
-        pass
+def triangularSystem(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    return backwardSubstitution(A, b)
+
+def gauss(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    '''
+    Solving a linear system with Gauss
+    '''
+    A, b = triangularize(A, b)
+    return triangularSystem(A, b)
+
+def LUDecomposition(A: np.ndarray) -> np.ndarray:
+    '''
+    Direct Method.
+    '''
+
+    U: np.ndarray = A.copy()
+    dimension: int = len(U)
+    L: np.ndarray = np.eye(dimension)
+    
+    for pivot in range(0, dimension):
+        bestPivot(U, None, pivot, dimension)
+        for row in range(pivot+1, dimension):
+            coeff: float = U[row][pivot] / U[pivot][pivot]
+            U[row] = U[row] - coeff * U[pivot]
+            L[row][pivot] = coeff
+    return L, U
+
+def jacobi() -> None:
+    '''
+    Iterative Method.
+    '''
+    
+    pass
+
+def gaussSeidel() -> None:
+    '''
+    Iterative Method.
+    '''
+    pass
--- Main.py
+++ Main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 import numpy as np
 
-from LinearSystems import LinearSystems
+import LinearSystems
 
-A = np.array([[1, 2, 3], [4, 5, 6], [5, 8, 9]], dtype=float)
-b = np.array([1, 2, 3], dtype=float)
+# A = np.array([[1, 2, 3], [4, 5, 6], [5, 8, 9]], dtype=float)
+# b = np.array([1, 2, 3], dtype=float)
 
-solution: np.ndarray = LinearSystems(A, b).gauss()
-print(f"Linear System solved: {solution}")
+# solution: np.ndarray = LinearSystems(A, b).gauss()
+# print(f"Linear System solved: {solution}")
 
-sol_np: np.ndarray = np.linalg.solve(A, b)
-print(f"Numpy solution: {sol_np}")
+# sol_np: np.ndarray = np.linalg.solve(A, b)
+# print(f"Numpy solution: {sol_np}")
 
 # A = np.array([[1, 2, 3], [4, 5, 6], [5, 8, 9]], dtype=float)
 # A: np.ndarray = np.array([[1, 2, 0, 1], [1, 2, 1, 0], [0, 1, 0, 1], [1, 0, 0, 1]], dtype=float)
@@ -19,7 +19,8 @@ A = np.array([
     [4, 4, 2],
     [2, 2, 3]
 ], dtype=float)
-decomposition: np.ndarray = LinearSystems(A, None).LUDecomposition()
+
+decomposition = LinearSystems.LUDecomposition(A)
 print(f"L: {decomposition[0]}")
 print(f"U: {decomposition[1]}")
 
